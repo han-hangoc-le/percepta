11cffdcd597f3d018a6a6866becde55042a3aeda
Alex-444-Tuan <anhtuanteo2006@gmail.com>
2025-11-07 22:23:43 -0500

    Initial Commit

---
+++ Percepta/PerceptaApp.swift
@@ -0,0 +1,32 @@
+//
+//  PerceptaApp.swift
+//  Percepta
+//
+//  Created by Anh Tuấn Trần on 11/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PerceptaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PerceptaTests/PerceptaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptaTests.swift
+//  PerceptaTests
+//
+//  Created by Anh Tuấn Trần on 11/7/25.
+//
+
+import Testing
+@testable import Percepta
+
+struct PerceptaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
